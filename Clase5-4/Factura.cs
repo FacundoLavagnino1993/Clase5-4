09e1d55a8082fabe153ef5e45cb40c55710f71d7
FacundoLavagnino1993 <facu9396@gmail.com>
2016-04-05 10:48:23 -0300

    Sobrecarga de metodos/operadores

---
+++ Clase5-4/Factura.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase5_4
+{
+    class Factura
+    {
+        //Sobrecarga de metodos:
+            // Crear metodos de igual nombre que reciban distintos tipos de parametros.
+            
+        DateTime fecha;
+        public int numero;
+        public float importe;
+
+
+        public Factura()
+        {
+            this.fecha = DateTime.Now;//Se obtiene la fecha actual.
+        }
+
+        public Factura(int numero, float importe):this()// Con este this llamamos al constructor de arriba iniciando la fecha en los demas constructores.
+        {
+            
+            this.numero = numero;
+            this.importe = importe;
+        }
+
+        // Se cambia el orden de los parametros.
+
+        public Factura(float importe, int numero):this(numero,importe)//Se utiliza el this, se le pasan los parametros del constructor de arriba 
+        {                                                             // Con los mismos nombres de los parametros que recibe el constructor que invoca al this.
+            
+            //this.importe = importe;  Se omiten las lineas comentadas para no repetir lineas.
+            //this.numero = numero;
+        }
+
+
+        //
+        public Factura(DateTime fecha, int numero)
+        {
+            this.fecha = fecha;
+            this.numero = numero;
+        }
+
+        public Factura(int numero, DateTime fecha)
+            : this(fecha, numero)
+        { }
+
+        public Factura(float importe, DateTime fecha)
+        {
+            this.importe = importe;
+            this.fecha = fecha;
+        }
+
+        public Factura(DateTime fecha, float importe)
+            : this(importe, fecha)
+        { }
+
+
+    }
+}
---
+++ Clase5-4/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase5_4
+{
+    class Dato
+    {
+       public int numero;
+    
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float Resultado;
+   
+            Console.WriteLine();//Recibe distintos parametros y todo lo transforma en string.
+            Factura FacturaUno = new Factura(20, 5f);
+            Factura FacturaDos = new Factura(5f, 20);
+
+            /*  ESTAS LINEAS DE CODIGO NO FUNCIONAN!!!
+             * 
+                Resultado = FacturaUno + FacturaDos;
+
+                if (FacturaUno == FacturaDos)
+                {
+                    Console.WriteLine("Tienen el mismo importe");
+                }
+             *   ESTAS LINEAS DE CODIGO NO FUNCIONAN!!!
+             */
+
+            Dato a = new Dato();
+            Dato b = new Dato();
+
+            a.numero = 22;
+            b.numero = 30;
+
+            a = b;
+            a.numero = 10;
+
+
+
+
+
+
+        }
+    }
+}
